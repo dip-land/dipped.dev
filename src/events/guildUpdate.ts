6b593739a625ab94b227ee021034f6b83389c9e7
dip-land <82669765+dip-land@users.noreply.github.com>
2024-05-31 18:58:00 -0500

    v2.1.4
    Fix small bug

--- src/events/guildUpdate.ts
+++ src/events/guildUpdate.ts
@@ -6,8 +6,6 @@ export default new Event({
     name: 'guildUpdate',
     on: true,
     async fn(oldGuild: Guild, newGuild: Guild) {
-        console.log('good');
-        await editGuildInfo(newGuild.id, { name: 'test' });
         await editGuildInfo(newGuild.id, { name: newGuild.name, icon: newGuild.iconURL() });
     },
 });
