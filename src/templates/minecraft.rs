90134a6fec570733923bb8fe3cf939539bdb137c
dip-land <82669765+dip-land@users.noreply.github.com>
2026-01-10 10:10:34 -0600

    Remove old card from new minecraft server card
    Removed the old html macro server card from the new minecraft server card.

--- src/templates/minecraft.rs
+++ src/templates/minecraft.rs
@@ -1,6 +1,9 @@
-use maud::{Markup, html};
+use maud::Markup;
 
-use crate::{minecraft::api::{Server, ServerStatus}, templates::terminal::{self, TerminalCardOptions}};
+use crate::{
+    minecraft::api::{Server, ServerStatus},
+    templates::terminal::{self, TerminalCardOptions},
+};
 
 pub fn server_card(server: Server) -> Markup {
     let classlist;
@@ -11,21 +14,12 @@ pub fn server_card(server: Server) -> Markup {
     } else {
         classlist = "server deleted";
     }
-    html! {
-        a class=(classlist) href=(format!("/minecraft/{}", server.identifier)) {
-            div class="server_background" style=(format!("background-image: url('/api/minecraft/icons/{}');", server.id)) {}
-            div class="server_content" {
-                img class="server_icon" src=(format!("/api/minecraft/icons/{}", server.id)) width="140px" height="140px" alt=(server.name) {}
-                span class="server_name" { (server.name) }
-            }
-        }
-    };
     terminal::terminal_card(TerminalCardOptions {
         classlist,
         href: &format!("/minecraft/{}", server.identifier),
         background_src: &format!("/api/minecraft/icons/{}", server.id),
         icon_src: &format!("/api/minecraft/icons/{}", server.id),
         icon_alt: &server.name,
-        content: &server.name
+        content: &server.name,
     })
 }
